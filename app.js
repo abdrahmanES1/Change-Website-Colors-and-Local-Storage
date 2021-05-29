dc4ed746b06469c2b5304ddd43863b3aa8dfb15e
abdrahmanES1 <68553016+abdrahmanES1@users.noreply.github.com>
2021-05-29 23:36:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 var el = document.querySelectorAll('.color-switcher li'),
     colors = [],
     localColor = localStorage.getItem('pagecolor');
-
+  
+ // get color from localStorage
 document.body.classList.add(localColor);
+
 
 for(let index = 0; index < el.length; index++) {
     
+    // Get Colors From  Attribute : data-color
     colors.push(el[index].getAttribute('data-color'));
 
+    // Add Event Click
     el[index].addEventListener('click', function() {
 
-        //remove all classList names
+        // emove all classList names
         document.body.classList.remove(...colors);
-        
+
+        // set color name in class of body 
         document.body.classList.add(this.getAttribute('data-color'));
 
         // add color value in localStorage
